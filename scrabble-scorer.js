60811edb96c05a4f9040223ee98f156dbce1e07c
jess <zamora.jesslyn@gmail.com>
2021-07-01 22:10:01 +0000

    changing scoringFucntion

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -78,19 +78,19 @@ const scoringAlgorithms = [
     {
       name: "Simple Score",
       description: "Each letter is worth 1 point.",
-      scoreFunction: simpleScore
+      scoringFunction: simpleScore
         
     },
     {
       name: "Bonus Vowels",
       description: "Vowels are 3 pts, consonants are 1 pt.",
-      scoreFunction: vowelBonusScore
+      scoringFunction: vowelBonusScore
       
     },
     {
       name: "Scrabble",
       description: "The traditional scoring algorithm.",
-      scoreFunction: scrabbleScore
+      scoringFunction: scrabbleScore
     }  
 ];
 
